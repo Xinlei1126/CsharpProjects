e3d20c6dd07861d16f2f865f06f4236de1f3632f
xinlei <guslixir@student.gu.se>
2026-09-03 15:20:43 +0200

    Update files

---
+++ week36/ProductList/ProductList/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter product names and type 'exit' to finish. All products will be displayed after exiting.");
+
+List<string> products = new List<string>();
+
+
+while (true)
+{
+    Console.WriteLine("Enter a product name: ");
+
+    if()
+
+}
+
+
+Console.WriteLine("All entered products: ");
+foreach (var i in products)
+{
+    Console.WriteLine("Element: " + i);
+}
+
+Console.ReadLine
